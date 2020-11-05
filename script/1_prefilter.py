bc441af9100727598475793e5ead2d3ae6c2e764
Lucas Batier <55487258+lucasbat20@users.noreply.github.com>
2020-11-05 16:42:26 +0100

    Update 1_prefilter.py

--- script/1_prefilter.py
+++ script/1_prefilter.py
@@ -13,17 +13,22 @@ args=parser.parse_args()
 update_progress(0)
 
 # No mowing
+# Select only meadows 13, grasslands 18 and woody moorlands 19
+# Format each not mowed parcels (pixels) to 0 and everywhere else to white 255
 GRT0 = imread(args.class0)
 GRT0[np.logical_and(GRT0 != 13, GRT0 != 18, GRT0 != 19)] = 255
 GRT0[np.logical_or(GRT0 == 13, GRT0 == 18, GRT0 == 19)] = 0
 
+# Save the image class 0
 imsave(args.class0, GRT0.astype(np.uint8))
 
 # Mowing
+# Format each mowed parcels (pixels) to 1 and everywhere else to white 255
 GRT1 = imread(args.class1)
 GRT1[GRT1 > 0] = 1
 GRT1[GRT1 == 0] = 255
 
+# Save the image class 1
 imsave(args.class1, GRT1.astype(np.uint8))
 
 update_progress(1)
